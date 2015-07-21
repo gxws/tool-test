655bed291d19709b04a868cb56578b25d4458695
朱伟亮 <zhuwl120820@gxwsxx.com>
2015-07-21 10:28:10 +0800

    初始化

---
+++ src/main/java/com/gxws/tool/test/data/annotation/Random.java
@@ -0,0 +1,21 @@
+package com.gxws.tool.test.data.annotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author zhuwl120820@gxwsxx.com
+ * @since 1.0
+ */
+
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Inherited
+public @interface Random {
+
+}
